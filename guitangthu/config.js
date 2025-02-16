f8b5fd6fd13454d5cf9eb4c49b82e8fee485eddd
duy-hung2008 <116080879+duy-hung2008@users.noreply.github.com>
2025-02-16 23:39:55 +0700

    Add files via upload

---
+++ guitangthu/config.js
@@ -0,0 +1,14 @@
+const nameGirl = '' ;
+const giftUrl = 'link nhg ko có j :)))';
+const eventName = 'Chúc Mừng SINH NHẬT THƯ';
+const titleCard = 'Gửi tặng tới Thư' ;
+const contentCard = `Chúc mừng sinh nhật Thư nhé! 💐
+Chúc Thư sang tuổi mới sẽ xinh đẹp hơn, học giỏi hơn và sẽ luôn có những điều may mắn với những điều tốt đẹp nhất tới với Thư nhé !`;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
