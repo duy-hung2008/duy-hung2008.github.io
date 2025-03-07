f8681c10bf0351fe9cb1806764c975529a9a0308
duy-hung2008 <116080879+duy-hung2008@users.noreply.github.com>
2025-03-08 00:02:41 +0700

    Add files via upload

---
+++ tangchoem/config.js
@@ -0,0 +1,14 @@
+const nameGirl = '' ;
+const giftUrl = 'link nhg ko có j :)))';
+const eventName = '8/3 vui vẻ nhaaanhaaa';
+const titleCard = 'Gửi tặng tới NhiNhi' ;
+const contentCard = `Chúc mừng 8/3 nhé! 💐
+Hôm nay là 8/3 là ngày của em. Nhi à chúng ta yêu xa nên hiện tại cx ko trực tiếp đưa qua cho e được nên e thông cảm ở điều này. Vì ko đưa được quà nên ah đã nghĩ ra cái này để lm cho em. Nhân dịp 8/3 anh chúc e ngày càng xinh đẹp, ngày càng học giỏi, những điều may mắn nhắt sẽ tới với e và tất cả nỗi buồn của e sẽ biến mất nhaaaa. IU EM NHÌU LẮM NEKKKKNEKKKK`;
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
